73487d9768779ec5c2548aba3e6b5375317771d9
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-03-04 16:10:11 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"bufio"
 	"flag"
-  "sync"
+	"sync"
 	"fmt"
-  "time"
+	"time"
 	"os"
 	"strconv"
 	"strings"
-  "net/http"
-  "log"
+	"net/http"
+	"log"
 )
 
 func main () {
-  var concurrency = 10
-  for i:=0; i<*concurrency/2; i++ {
-    wg.Add(1)
-    go func() {
-      time.Sleep(time.Millisecond * 50)
-      wg.Done()
-    }()
-    wg.Wait()
-  }
+	stat := time-now()
+	var counter = 0
+	var concurrency = 10
+	for i:=1; i<=concurrency; i++ {
+		go func() {
+			time.Sleep(time.Millisecond * 50)
+			counter++
+		}()
+	}
 }
 
 var googleUrls = map[string]string{
 	"com": "https://www.google.com/search?q=",
-  "de": "https://www.google.de/search?q=",
+ 	"de": "https://www.google.de/search?q=",
 	"uk": "https://www.google.co.uk/search?q=",
 	"fr": "https://www.google.fr/search?q=",
 }
