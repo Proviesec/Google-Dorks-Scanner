015ebdb474d964c27524d4294c82ebe34eb0f314
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2022-03-09 16:59:07 +0100

    Update google-search.go

--- google-search.go
+++ google-search.go
@@ -13,3 +13,19 @@ type Result struct {
 	Title string `json:"title"`
 	Description string `json:"description"`
 }
+type SearchOptions struct {
+	CountryCode string
+	LanguageCode string
+	Limit int
+	Start int
+	// Default: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+	UserAgent string
+	ProxyAddr string
+}
+
+var googleUrls = map[string]string{
+	"com": "https://www.google.com/search?q=",
+ 	"de": "https://www.google.de/search?q=",
+	"uk": "https://www.google.co.uk/search?q=",
+	"fr": "https://www.google.fr/search?q=",
+}
